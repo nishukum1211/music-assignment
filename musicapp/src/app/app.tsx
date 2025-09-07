80521560f5af623638aa5264e480af82c4d02133
Nishu <nishu.kumari@aitoxr.com>
2025-09-08 00:24:56 +0530

    minor fix

--- musicapp/src/app/app.tsx
+++ musicapp/src/app/app.tsx
@@ -4,7 +4,7 @@ import './styles.css';
 
 export function App() {
   return (
-    <div className="min-h-screen grid place-items-center bg-gradient-to-br from-gray-900 via-purple-900 to-black">
+    <div className=" bg-gradient-to-br from-gray-900 via-purple-900 to-black">
       <MusicContainer role="user" />
     </div>
   );
